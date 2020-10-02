2a44015faa5e1c56e079dab22bc5444d0eb96fb8
Kamalakar (Raunak) Srivastava <iamrnkk@gmail.com>
2020-10-02 21:38:20 +0530

    Add functioning

--- game.js
+++ game.js
@@ -1,37 +1,79 @@
-var color=["green","red","blue","yellow"]
-var sequence=[];
-var stage=0;
-var gameOver=false;
-document.addEventListener("keypress",gameStart)
 
-function gameStart()
-{
-  stage++;
-  if (gameOver===false)
-   {
-   console.log("works");
-   var x= Math.ceil(Math.random()*4)-1;
-   console.log(x);
-   sequence.push(color[x]);
-    console.log(sequence);
-    $("#level-title").html("level "+stage);
-    var currentColor= sequence[stage-1];
-    $("#"+currentColor).fadeIn(100).fadeOut(100).fadeIn(100);
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-      // if (sequence[stage-1]==) {
-      //   $(".green").fadeOut().delay(100).fadeIn();
-      // }
-      // else if (sequence[stage-1]==2) {
-      //   $(".red").fadeOut().delay(100).fadeIn();
-      //   }
-      //   else if (sequence[stage-1]==3) {
-      //     $(".blue").fadeOut().delay(100).fadeIn();
-      //   }
-      //   else{
-      //     $(".yellow").fadeOut().delay(100).fadeIn();
-      //   }
+var gamePattern = [];
+var userClickedPattern = [];
 
+var started = false;
+var level = 0;
 
-
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
   }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
