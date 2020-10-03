c920c49c3c138f03eb31ec7259a9502ea3143671
Kamalakar (Raunak) Srivastava <iamrnkk@gmail.com>
2020-10-03 17:47:54 +0530

    Bug Fix

--- game.js
+++ game.js
@@ -7,21 +7,13 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
+// Disable Dafault events
 $(document).keydown(function(e){
     if(e.which==32 ||e.which==13) e.preventDefault();
 });
-$('#level-title').click(function() {
-  if (!started) {
-    $('#level-title').fadeOut(1000, function()
-    {
-        $(this).html("level " +level).fadeIn(1000);
-    });
-    setTimeout(function () {
-      nextSequence();
-    }, 1200);
-    started = true;
-  }
-});
+
+// handle events
+
 
 $('.start').click(function() {
   $(".start").animate({
@@ -30,24 +22,35 @@ $('.start').click(function() {
   },500);
 
   if ($(window).width() >= 600) {
+    // Desktop layout
   $(".colors").delay(400).animate({
-      // right: "+=30px",
       margin: '+=23px',
       height: '+=150px',
       width: '+=150px'
     },1500);
-    
-   }
 
+   }
+    // Mobile layout
    if ($(window).width() < 600) {
      $(".colors").delay(400).animate({
-      // right: "+=30px",
       margin: '+=3px',
       height: '+=35px',
       width: '+=35px'
     },1500);
     }
-    
+    $('#level-title').click(function() {
+      if (!started) {
+        $('#level-title').fadeOut(1000, function()
+        {
+            $(this).html("level " +level).fadeIn(1000);
+        });
+        setTimeout(function () {
+          nextSequence();
+        }, 1200);
+        started = true;
+      }
+    });
+
     $('#level-title').fadeOut(1000, function()
     {
         $(this).html("Click here to Start").fadeIn(1000);
@@ -66,7 +69,7 @@ $(".colors").click(function() {
   checkAnswer(userClickedPattern.length-1);
 }
 });
-
+// Check User and Randomly generated sequence
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
@@ -88,7 +91,7 @@ function checkAnswer(currentLevel) {
     }
 }
 
-
+// Generating next sequence
 function nextSequence() {
   userClickedPattern = [];
   level++;
@@ -100,7 +103,7 @@ function nextSequence() {
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
-
+// Animate Buttons
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
@@ -112,7 +115,7 @@ function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
-
+// Reset
 function startOver() {
   level = 0;
   gamePattern = [];
